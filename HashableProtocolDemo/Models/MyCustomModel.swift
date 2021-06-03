8f803c11bc6e76088710bf0e31f2686f0df48431
Frederick Javalera <frederickjavalera@gmail.com>
2021-06-02 18:37:15 -0600

    - Add hash function.

--- HashableProtocolDemo/Models/MyCustomModel.swift
+++ HashableProtocolDemo/Models/MyCustomModel.swift
@@ -9,8 +9,19 @@ import Foundation
 
 struct MyCustomModel: Hashable {
   
+  // MARK: Properties
 //  Required for using Identifiable Protocol instead of Hashable
 //  let id = UUID().uuidString
   
   let title: String
+  
+  /*
+   Can be used to create combined hash values. Example below
+   combines then hashes title and subtitle.
+   */
+//  MARK: Methods
+//  let subtitle: String
+//  func hash(into hasher: inout Hasher) {
+//    hasher.combine(title + subtitle)
+//  }
 }
